87e715449f36431254a9beea423cd5bac6691e52
prki <mar.pikna@gmail.com>
2023-08-10 09:01:03 +0100

    Participant challonge_user_id

--- pkg/challonge/participants.go
+++ pkg/challonge/participants.go
@@ -5,6 +5,7 @@ type Participant struct {
 	Name              string `json:"name"`
 	FinalRank         int    `json:"final_rank"`
 	ChallongeUsername string `json:"challonge_username"`
+	ChallongeUserId   int    `json:"challonge_user_id"`
 	DisplayName       string `json:"display_name"`
 }
 
